39f789bfaad37998e0959b23fe123ec48babdf8b
João Victor Correa <correa.joaov25@gmail.com>
2024-01-08 14:56:17 -0300

    closes #4 - Implementar as 4 operações matemáticas

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-function calc() {
+function calc(operator) {
     
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
+    var output = 0.0;
 
-    var output = num1 + num2;
+    switch (operator) {
+        
+        case '+': output = num1 + num2; break;
+        case '-': output = num1 - num2; break;
+        case '*': output = num1 * num2; break;
+        case '/': output = num1 / num2; break;
+    }
 
     document.getElementById("output").value = output;
 }
